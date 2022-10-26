f256c29af73816b214c5fafa3bc846459d61e842
Abu Md Nayeem Khan <abumdnayeemkhan@gmail.com>
2022-10-26 12:26:11 +0600

    Learning javascript

---
+++ app.js
@@ -0,0 +1,14 @@
+//====================DISPLAY POSSIBILITIES====================//
+// innerHtml, Document. Write(), window.alert(), console.log();
+
+let show = document.getElementById("show");
+
+let a = "Abu Md ";
+let b = "Nayeem Khan";
+let x = a + b;
+
+
+// show.innerHTML = x; // DISPLAYING BY INNNER HTML
+// document.write(); // INSIDE IN HTML
+// window.alert(x); // PRINT BY ALERT
+// console.log(x); // PRINT IN CONSOLE
